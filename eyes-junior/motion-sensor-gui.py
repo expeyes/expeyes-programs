5a0bf506db914a4a8815cb68633d2094ef6c8897
Praveen (Gnovi) <wavicles@users.noreply.github.com>
2015-06-04 23:47:37 +0530

    Changes for Python 3

--- eyes-junior/motion-sensor-gui.py
+++ eyes-junior/motion-sensor-gui.py
@@ -1,5 +1,10 @@
-''
-expEYES program for interfacing SRF05 motion sensor 
+'''
+expEYES program
+Author  : Praveen Patil
+Developed as a part of GSoC- project  
+License : GNU GPL version 3
+
+Program to interface SRF-05 motion sensor and fetch data
 
 '''
 import gettext
@@ -7,8 +12,19 @@ gettext.bindtextdomain("expeyes")
 gettext.textdomain('expeyes')
 _ = gettext.gettext
 
-from Tkinter import *
-import expeyes.eyesj as eyes, expeyes.eyeplot as eyeplot, expeyes.eyemath as eyemath, time, sys, math
+
+import time, math, sys
+if sys.version_info.major==3:
+        from tkinter import *
+else:
+        from Tkinter import *
+
+sys.path=[".."] + sys.path
+
+import expeyes.eyesj as eyes
+import expeyes.eyeplot as eyeplot
+import expeyes.eyemath as eyemath
+
 
 WIDTH = 600 # width of drawing canvas
 HEIGHT = 400 # height
