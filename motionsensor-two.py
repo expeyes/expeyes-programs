b6b46076ec42c08e4474646ac5d5a01459de8a02
Praveen (Gnovi) <wavicles@users.noreply.github.com>
2015-06-07 21:47:39 +0530

    Changes for python 3

--- motionsensor-two.py
+++ motionsensor-two.py
@@ -8,6 +8,18 @@ gettext.bindtextdomain("expeyes")
 gettext.textdomain('expeyes')
 _ = gettext.gettext
 
+from Tkinter import *
+import time, sys, math
+if sys.version_info.major==3:
+from tkinter import *
+else:
+from Tkinter import *
+sys.path=[".."] + sys.path
+import expeyes.eyesj as eyes
+import expeyes.eyeplot as eyeplot
+import expeyes.eyemath as eyemath
+
+
 
 WIDTH = 600 # width of drawing canvas
 HEIGHT = 400 # height
