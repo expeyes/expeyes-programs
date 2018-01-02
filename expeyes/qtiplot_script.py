85f885725eec1e4791c58cc58ddf3e03a350d221
Georges Khaznadar <georgesk@debian.org>
2018-01-02 18:45:20 +0100

    qti plotting begins to be responsive

--- expeyes/qtiplot_script.py
+++ expeyes/qtiplot_script.py
@@ -17,7 +17,7 @@ while l:
 cols=max([len(l) for l in data])
 
 rows=len(data)
-t = newTable("Mes données", rows, cols)
+t = newTable("Table1", rows, cols)
 t.setColNames(names)
 
 row=1
@@ -29,8 +29,10 @@ for l in data:
 	row+=1
 
 colTuple=tuple(names)
-#g1=plot(table("Mes données"), colTuple, Layer.LineSymbols)
 
-#l=g1.activeLayer()
-#l.setTitle(dt.strftime("%Y-%m-%d %H:%M:%S"))
+g = newGraph()
+l=g.activeLayer()
+l.insertCurve(t, "col1", "col2", Layer.LineSymbols)
+
+l.setTitle(dt.strftime("%Y-%m-%d %H:%M:%S"))
 
