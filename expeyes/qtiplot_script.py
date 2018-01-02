eb5a7e4afb7c5b79ca9096a27642ed648a5489ea
Georges Khaznadar <georgesk@debian.org>
2018-01-02 18:16:04 +0100

    now qtiplot creates at least a table with data

--- expeyes/qtiplot_script.py
+++ expeyes/qtiplot_script.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
 import sys, re, codecs, datetime
 
+separators=re.compile(r"\s+")
 dt=datetime.datetime.now()
 data=[]
 
+# read column names from stdin
 l=sys.stdin.readline().strip()
-separators=re.compile(r"\s+")
 names=separators.split(l)
+
 while l:
-	l=sys.stdin.readline().strip()
+        l=sys.stdin.readline().strip()
 	if l:
 		data.append(separators.split(l))
 	
 cols=max([len(l) for l in data])
+
 rows=len(data)
 t = newTable("Mes données", rows, cols)
 t.setColNames(names)
@@ -21,13 +24,13 @@ row=1
 for l in data:
 	col=1
 	for val in l:
-		t.setCellData(col,row,val)
+		t.setCellData(col,row,float(val))
 		col+=1
 	row+=1
 
 colTuple=tuple(names)
-g1=plot(table("Mes données"), colTuple, Layer.LineSymbols)
+#g1=plot(table("Mes données"), colTuple, Layer.LineSymbols)
 
-l=g1.activeLayer()
-l.setTitle(dt.strftime("%Y-%m-%d %H:%M:%S"))
+#l=g1.activeLayer()
+#l.setTitle(dt.strftime("%Y-%m-%d %H:%M:%S"))
 
