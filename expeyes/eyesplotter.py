c4b93b9e23aafece4bd55820e6867963e09eacaa
Georges Khaznadar <georgesk@debian.org>
2018-05-25 16:29:48 +0200

    refactored the code

--- expeyes/eyesplotter.py
+++ expeyes/eyesplotter.py
@@ -27,66 +27,125 @@ ydataTemplate="t.setColData({col}, {ydata})"
 
 curveTemplate="c{num}=l.insertCurve(t, '1', '{num}', Layer.Line, {color}, -1)"
 
+class Exporter:
+    List=[]
+    def __init__(self, expFunc, label, tooltip=""):
+        """
+        Creates an exporter instance and register it in Exporter.List
+        @param expFunc a function with the profile:
+        title, xlabel, ylabel, xdata, ydata -> list of files
+        which calls an application in the background and returns a
+        list of temporary files to erase.
+        @param label a short description
+        @param tooltip a longer description
+        """
+        self.func=expFunc
+        self.label=label
+        self.tooltip=tooltip
+        Exporter.List.append(self)
+        return
+
+def grace(title, xlabel, ylabel, xdata, ydata):
+    print("DEBUG: should call xmgrace")
+    return []
+
+def qtiplot(title, xlabel, ylabel, xdata, ydata):
+    print("DEBUG: calling qtiplot with title", title)
+    from subprocess import call
+    from tempfile import NamedTemporaryFile
+    rows=len(xdata)
+    xdata=str(list(xdata))
+    if len(ydata.shape)==1:
+        #simple plot
+        cols=2
+        templatedYdata=ydataTemplate.format(
+            col=2,
+            ydata=str(list(ydata)),
+        )
+        TemplatedCurve=curveTemplate.format(
+            num=2,
+            color=0,
+        )
+    else:
+        for i in range(ydata.shape[0]):
+            #multiple plots
+            cols=ydata.shape[0]+1
+            yd=[]
+            for i in range(cols-1):
+                yd.append(
+                    ydataTemplate.format(
+                        col=2+i,
+                        ydata=str(list(ydata[i])),
+                    ))
+            templatedYdata="\n".join(yd)
+            c=[]
+            for i in range(cols-1):
+                c.append(
+                    curveTemplate.format(
+                        num=2+i,
+                        color=i,
+                    ))
+            TemplatedCurve="\n".join(c)
+    script=qtiScript.format(
+        rows=rows, cols=cols, xdata=xdata,
+        templatedYdata=templatedYdata, TemplatedCurve=TemplatedCurve,
+        title=title,
+        xlabel=xlabel,
+        ylabel=ylabel,
+    )
+    temp=NamedTemporaryFile(mode="w", prefix="qti_", delete=False)
+    temp.write(script)
+    temp.close()
+    call("(qtiplot --execute {temp}&)".format(temp=temp.name), shell=True)
+    return [temp]
+
+class ExportButton(QPushButton):
+    """
+    implements a push button which can access data from a PlotWindow
+    and export them to an external application, launched in the
+    background.
+    """
+    def __init__(self, parent, exporter):
+        """
+        @param parent supposedly a PlotWindow instance
+        @param exporter an Exporter instance
+        """
+        assert (isinstance(parent, PlotWindow))
+        assert (isinstance(exporter, Exporter))
+        QPushButton.__init__(self, exporter.label, parent)
+        self.pw=parent
+        self.cb=exporter.func
+        self.setToolTip(exporter.tooltip)
+        self.clicked.connect(self.export)
+        return
+    
+    def export(self):
+        self.pw.tmpFiles+=self.cb(self.pw.title,
+                                  self.pw.xlabel, self.pw.ylabel,
+                                  self.pw.xdata, self.pw.ydata)
+        return
+
+
+"""
+The supported export modes. Instances are created but not forgotten, since
+they are appended to Exporter.List immediately
+"""
+Exporter(
+    qtiplot,
+    _translate("eyesplotter","Qtiplot"),
+    _translate("eyesplotter","Modern plotter/analyzer")
+)
+Exporter(
+    grace,
+    _translate("eyesplotter","Grace"),
+    _translate("eyesplotter","Fast old-fashioned plotter/analyzer")
+)    
+
 class PlotWindow(QWidget):
     """
     Implements a quick plot window, which can export its data to a few other 
     plotting applications
     """
-    def grace(self):
-        print("DEBUG: should call xmgrace")
-        return
-
-    def qtiplot(self):
-        print("DEBUG: calling qtiplot with title", self.title)
-        from subprocess import call
-        from tempfile import NamedTemporaryFile
-        rows=len(self.xdata)
-        xdata=str(list(self.xdata))
-        if len(self.ydata.shape)==1:
-            #simple plot
-            cols=2
-            templatedYdata=ydataTemplate.format(
-                col=2,
-                ydata=str(list(self.ydata)),
-            )
-            TemplatedCurve=curveTemplate.format(
-                num=2,
-                color=0,
-            )
-        else:
-            for i in range(self.ydata.shape[0]):
-                #multiple plots
-                cols=self.ydata.shape[0]+1
-                yd=[]
-                for i in range(cols-1):
-                    yd.append(
-                        ydataTemplate.format(
-                            col=2+i,
-                            ydata=str(list(self.ydata[i])),
-                        ))
-                templatedYdata="\n".join(yd)
-                c=[]
-                for i in range(cols-1):
-                    c.append(
-                        curveTemplate.format(
-                            num=2+i,
-                            color=i,
-                        ))
-                TemplatedCurve="\n".join(c)
-        script=qtiScript.format(
-            rows=rows, cols=cols, xdata=xdata,
-            templatedYdata=templatedYdata, TemplatedCurve=TemplatedCurve,
-            title=self.title,
-            xlabel=self.xlabel,
-            ylabel=self.ylabel,
-        )
-        temp=NamedTemporaryFile(mode="w", prefix="qti_", delete=False)
-        temp.write(script)
-        temp.close()
-        self.tmpFiles.append(temp)
-        call("(qtiplot --execute {temp}&)".format(temp=temp.name), shell=True)
-        return
-
     def closeEvent(self, event):
         from os import unlink
         QWidget.closeEvent(self, event)
@@ -94,22 +153,6 @@ class PlotWindow(QWidget):
             unlink(temp.name)
         return
 
-    """
-    dictionary of supported export modes.
-
-    the keys must be the name of a method in PlotWindow, which
-    will inherit self.xdata and the like to launch an external application
-    in the background and plot there the same as in the PlotWindow.
-
-    Every key is associated with a tuple of strings, for a button label
-    and its toolTip.
-    """
-    exportModes={
-        "qtiplot": (_translate("eyesplotter","Qtiplot"),
-                    _translate("eyesplotter","Modern plotter/analyzer")),
-        "grace": (_translate("eyesplotter","Grace"),
-                  _translate("eyesplotter","Fast old-fashioned plotter/analyzer")),
-        }
     def __init__(self, parent=None,
                  xdata=[], ydata=[], xlabel="", ylabel="",
                  title=""):
@@ -144,18 +187,14 @@ class PlotWindow(QWidget):
                 #multiple plots
                 plotWidget.plot(x, y[i], pen=(i,3))
         # plot goes on top, spanning all columns
-        layout.addWidget(plotWidget, 0, 0, 1,(1+len(self.exportModes)))  
+        layout.addWidget(plotWidget, 0, 0, 1,(1+len(Exporter.List)))  
 
         l= QLabel(_translate("eyesplotter","Export to"))
         layout.addWidget(l, 1, 0)
         col=1
-        for exp in self.exportModes:
-            label, toolTip = self.exportModes[exp]
-            procedure=getattr(self, exp)
-            btn=QPushButton(label)
-            btn.setToolTip(toolTip)
+        for exp in Exporter.List:
+            btn=ExportButton(self, exp)
             layout.addWidget(btn, 1, col)
-            btn.clicked.connect(procedure)  
             col +=1
         return
 
