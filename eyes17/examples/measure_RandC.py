914e9cc4728c014d769a539df37e10b771a95e22
Georges Khaznadar <georgesk@debian.org>
2018-07-21 15:28:45 +0200

    use python3's print function

--- eyes17/examples/measure_RandC.py
+++ eyes17/examples/measure_RandC.py
@@ -2,12 +2,13 @@
 Measures capacitance in the range of pico Farads
 '''
 
+from __future__ import print_function
 import eyes17.eyes
 p = eyes17.eyes.open()
 
 #connect Capacitor from IN1 to GND
-print p.get_capacitance()
+print (p.get_capacitance())
 
 #connect an LDR  from SEN to GND
-print p.get_resistance()
+print (p.get_resistance())
 
--- eyes17/examples/multi_r2rtime.py
+++ eyes17/examples/multi_r2rtime.py
@@ -3,10 +3,11 @@ multi_r2rtime(input, ncycles) returns the microseconds elapsed between two risin
 ncycles is the number of cycles to be measured.
 '''
 
+from __future__ import print_function
 import eyes17.eyes
 p = eyes17.eyes.open()
 
 p.set_sqr1(1000)               # set 1kHz on  SQR1
 t = p.multi_r2rtime('IN2')    # 6 is the readback of SQR1. Time of 10 cycles
 
-print t, 'Frequency = ', (1.0/t)
+print (t, 'Frequency = ', (1.0/t))
