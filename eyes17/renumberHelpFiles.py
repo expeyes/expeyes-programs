3d9c5307f16b4ecc8036baa05fe834d70b8d68ce
Georges Khaznadar <georgesk@debian.org>
2020-02-28 20:07:32 +0100

    improved the renumbering system

--- eyes17/renumberHelpFiles.py
+++ eyes17/renumberHelpFiles.py
@@ -1,13 +1,17 @@
 #! /usr/bin/python
 
 from glob import glob
-import os, re
+import os, re, subprocess
 
 paths=glob('../ExpEYES17/UserManual/??')
 
+subprocess.call ("rm -rf /tmp/new", shell=True)
+
 commands=[]
 for p in paths:
-    p=os.path.join(p,'rst', 'exp')
+    p=os.path.join(os.path.abspath(p),'rst', 'exp')
+    d=os.path.join("/tmp/new", re.sub(r".*UserManual/", "", p))
+    subprocess.call("mkdir -p "+d, shell=True)
     with open("renumbering.txt") as rn:
         for l in rn.readlines():
             l=l.strip()
@@ -15,8 +19,7 @@ for p in paths:
             if m:
                 f=m.group(1)+".rst"
                 g=m.group(2)+".rst"
-                commands.insert(0, f"git mv {os.path.join(p,f)} {os.path.join(p,g)}")
-
-for l in commands:
-    print(l)
+                subprocess.call(f"cp {os.path.join(p,f)} {os.path.join(d,g)}", shell=True)
                 
+print("done, look at: /tmp/new")
+subprocess.call("pcmanfm /tmp/new", shell=True)
