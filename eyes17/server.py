94aea1b3253c9d265313700808463801db2d8d6e
Georges Khaznadar <georgesk@debian.org>
2020-07-25 20:21:53 +0200

    removed dead code

--- eyes17/server.py
+++ eyes17/server.py
@@ -1,6 +1,6 @@
 """
 EYES17 for Young Engineers and Scientists
-Python library to communicate by http-enabled sockets
+Python library to communicate screenshots through sockets, with HTTP
 Author  : Georges Khaznadar <georgesk@debian.org>
 License : GNU GPL version 3
 Started on 2020-07-25
@@ -13,59 +13,6 @@ from tempfile import NamedTemporaryFile
 import os
 
 INDEX = open("server.html").read()
-
-class EYESRequestHandler(http.server.BaseHTTPRequestHandler):
-
-    def do_GET(self):
-        if "?" in self.requestline:
-            # this is a request with parameters, let's parse them
-            _, params = self.requestline.split("?")
-            params = params.split("&")
-            vars={}
-            for p in params:
-                k,v = p.split("=")
-                vars[k] = v
-            # params have been parsed into the vars dictionary
-            message=""
-            width=0
-            if vars["format"] == "svg":
-                message += "format SVG; "
-            elif vars["format"] == "png":
-                message += "format PNG; "
-                try:
-                    width = int(vars["width"])
-                except:
-                    width = 400 # the default width value
-                message += f"width = {width}px; "
-            else:
-                message += "format UNKNOWN; "
-            if vars["shot"].startswith("Full"):
-                message += "full Eyes17 snapshot"
-            else:
-                message += "snapshot of the display only"
-            # now, send the response
-            self.send_response(200)
-            self.send_header('Content-type','text/html')
-            self.end_headers()
-            self.wfile.write(message.encode("utf-8"))
-        else: # no parameters, just show the index webpage
-            self.send_response(200)
-            self.send_header('Content-type','text/html')
-            self.end_headers()
-            self.wfile.write(INDEX.encode("utf-8"))
-        return
-
-def runserver(PORT = 45594):
-    """
-    Runs a http service on PORT 45594 by default
-    """
-    with socketserver.TCPServer(("", PORT), EYESRequestHandler) as httpd:
-        print("EYES17's schreenshot service, at port:", PORT)
-        try:
-            httpd.serve_forever()
-        except KeyboardInterrupt:
-            httpd.socket.close()
-    return
 
 class ScreenShotThread(QThread):
     def __init__(self, parent, port = 45594):
@@ -143,6 +90,3 @@ class ScreenShotThread(QThread):
                 print("Closing EYESRequestHandler, err =", err)
                 httpd.socket.close()
         return
-        
-if __name__ == "__main__":
-    runserver()
