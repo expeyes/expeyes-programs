8469d7947267766fb8f2d9927988a673524d6790
Georges Khaznadar <georgesk@debian.org>
2020-07-26 19:32:39 +0200

    first successful attempt to insert a SVG file

--- eyes17/lo_eyes17_plugin.py
+++ eyes17/lo_eyes17_plugin.py
@@ -6,26 +6,69 @@ License : GNU GPL version 3
 Started on 2020-07-25
 """
 
-import uno
-from com.sun.star.awt import Size, Point
+import unohelper, uno
+from com.sun.star.beans import PropertyValue
+from com.sun.star.awt import Size
 
-def add5Graphics():
-    oDesktop = XSCRIPTCONTEXT.getDesktop()
-    oDoc = oDesktop.getCurrentComponent()
-    if hasattr( oDoc, "Text" ):
-        oDrawPage = oDoc.getDrawPage()
+def svgFromEyes17():
+    desktop = XSCRIPTCONTEXT.getDesktop()
+    doc = desktop.getCurrentComponent()
+    if hasattr( doc, "Text" ):
+        drawPage = doc.getDrawPage()
 
-        oUndoManager = oDoc.getUndoManager()
-        oUndoManager.enterUndoContext( "Add Five Graphic Objects" )
+        undoManager = doc.getUndoManager()
+        undoManager.enterUndoContext( "Insert screenshot from Eyes17" )
 
-        from com.sun.star.awt import Size
-        from com.sun.star.awt import Point
+        # do the work here
+        ctx = uno.getComponentContext()
+        smgr = ctx.getServiceManager()
+        graphicprovider = smgr.createInstance("com.sun.star.graphic.GraphicProvider")
+        
+        dpi = 150
+        cursor = doc.Text.createTextCursor()
+        cursor.gotoEnd(False)
+        
+        # those might become parameters:
+        width = None
+        height = None
+        paraadjust = None
+        scale = 1000 * 2.54 / float(dpi)
+        istream = ctx.ServiceManager.createInstanceWithContext("com.sun.star.io.SequenceInputStream", ctx)
+        fbytes=b''
+        with open("/tmp/oscilloscope-screen-dark.svg", "rb") as f:
+            fbytes = f.read()
+        istream.initialize((uno.ByteSequence(fbytes),))
+        graphic = graphicprovider.queryGraphic((PropertyValue('InputStream', 0, istream, 0), ))
+        if graphic.SizePixel is None:
+            # Then we're likely dealing with vector graphics. Then we try to
+            # get the "real" size, which is enough information to
+            # determine the aspect ratio
+            original_size = graphic.Size100thMM
+        else:
+            original_size = graphic.SizePixel
+        graphic_object_shape = doc.createInstance('com.sun.star.drawing.GraphicObjectShape')
+        graphic_object_shape.Graphic = graphic
+        if width and height:
+            size = Size(int(width * scale), int(height * scale))
+        elif width:
+            size = Size(int(width * scale), int((float(width)/original_size.Width) * original_size.Height * scale))
+        elif height:
+            size = Size(int((float(height)/original_size.Height) * original_size.Width * scale), int(height * scale))
+        else:
+            size = Size(int(original_size.Width * scale), original_size.Height * scale)
+        graphic_object_shape.setSize(size)
+        # doc.Text.insertTextContent(cursor, graphic_object_shape, False)
+        thisgraphicobject = doc.createInstance("com.sun.star.text.TextGraphicObject")
+        thisgraphicobject.Graphic = graphic_object_shape.Graphic
+        thisgraphicobject.setSize(size)
+        if paraadjust:
+            oldparaadjust = cursor.ParaAdjust
+            cursor.ParaAdjust = paraadjust
+        doc.Text.insertTextContent(cursor, thisgraphicobject, False)
+        if paraadjust:
+            cursor.ParaAdjust = oldparaadjust
+        
+        undoManager.leaveUndoContext()
 
-        for i in range(5):
-            oGraphic = oDoc.createInstance( "com.sun.star.drawing.GraphicObjectShape" )
-            oGraphic.setSize( Size(3000,2000) )
-            oGraphic.setPosition( Point( i*3000, 1000 ) )
-            oDrawPage.add( oGraphic )
 
-        oUndoManager.leaveUndoContext()
         
