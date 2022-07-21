9c250c9603a2b018b3b2c949b8b4efe1efddf6d6
Georges Khaznadar <georgesk@debian.org>
2022-07-21 18:19:34 +0200

    fixed QRegRxp

--- eyes17/pythonSyntax.py
+++ eyes17/pythonSyntax.py
@@ -42,6 +42,7 @@ POSSIBILITY OF SUCH DAMAGE.
 import sys
 
 from QtVersion import *
+from PyQt6.QtCore import QRegularExpression
 
 def format(color, style=''):
     """Return a QTextCharFormat with the given attributes.
@@ -109,8 +110,8 @@ class PythonHighlighter (QSyntaxHighlighter):
         # Multi-line strings (expression, flag, style)
         # FIXME: The triple-quotes in these two lines will mess up the
         # syntax highlighting from this point onward
-        self.tri_single = (QRegExp("'''"), 1, STYLES['string2'])
-        self.tri_double = (QRegExp('"""'), 2, STYLES['string2'])
+        self.tri_single = (QRegularExpression("'''"), 1, STYLES['string2'])
+        self.tri_double = (QRegularExpression('"""'), 2, STYLES['string2'])
 
         rules = []
 
