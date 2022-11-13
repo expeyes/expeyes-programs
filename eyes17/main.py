ef7efb48b7f10434401f1da78d502d805d3ef85e
Georges Khaznadar <georgesk@debian.org>
2022-11-13 10:14:05 +0100

    removed the PYSIDE occurences

--- eyes17/main.py
+++ eyes17/main.py
@@ -65,12 +65,8 @@ class PQG_ImageExporter(Exporter):
 
     def export(self, fileName=None, toBytes=False, copy=False):
         if fileName is None and not toBytes and not copy:
-            if USE_PYSIDE:
-                filter = ["*."+str(f)
-                          for f in QtGui.QImageWriter.supportedImageFormats()]
-            else:
-                filter = ["*."+bytes(f).decode('utf-8')
-                          for f in QtGui.QImageWriter.supportedImageFormats()]
+            filter = ["*."+bytes(f).decode('utf-8')
+                      for f in QtGui.QImageWriter.supportedImageFormats()]
             preferred = ['*.png', '*.tif', '*.jpg']
             for p in preferred[::-1]:
                 if p in filter:
